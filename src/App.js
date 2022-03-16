3ffdcab6f8a8f8dcea8ba1b12c7948a060b52b28
benji694 <bbouillard@edenschool.fr>
2022-03-16 13:53:45 +0100

    TodoList react v.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleChange = e =>{
+    setInput(e.target.value);
+  };
+
+  let element = {id: Math.floor(Math.random()*10000), filter: 'todo', balise: <li><span>{input}</span> <button onClick={()=>{
+    todo.splice(todo.indexOf(this));
+  }}>delete</button></li>};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={input} type="text" placeholder='add a todo' name='text' onChange={handleChange}/>
+      <button onClick={()=>{
+        setTodo([...todo, element]);
+        setInput('');
+      }}>ADD</button>
+      <ul>
+        {todo.map((e)=>{return e.balise})}
+      </ul>
+      
     </div>
   );
 }
